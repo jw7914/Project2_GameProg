1041558b449a8390a1f75f5c21ec3fa495c19d69
Jason Wu <124104246+jw7914@users.noreply.github.com>
2024-10-16 17:20:12 -0400

    Update main.cpp

--- Project2/main.cpp
+++ Project2/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Jason Wu
+* Assignment: Pong Clone
+* Date due: 2024-10-12, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define GL_GLEXT_PROTOTYPES 1
